9b7a7714db8e21eb47d909f112a47713c108772e
MELLI2022 <denisenkoswetlana109@gmail.com>
2022-10-22 17:05:36 +0300

    Проверьте ДЗ ПЖ.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Car lada = new Car("", "", 0, "", 0, "");
+        System.out.println(lada);
+        Car audi = new Car("Audi", "A8 50 L TDI quattro", 3.0, " черный", 2020, "Германии");
+        System.out.println(audi);
+        Car bmv = new Car("BMW", "Z8", 3.0, "черный", 2021, "Германии");
+        System.out.println(bmv);
+        Car kia = new Car("Kia", "Sportage 4-го поколения",  2.4, " красный", 2018, "Южной Корее");
+        System.out.println(kia);
+        Car hyundai = new Car("Hyundai", "Avante", 1.6, "оранжевый", 2016, "Южной Корее");
+        System.out.println(hyundai);
+
     }
 }
